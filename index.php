a9b3ca89ddc57aa0b8d1620cfe44ca0e1e21138e
Fuad <kamilovfuad@gmail.com>
2022-03-29 20:00:11 +0400

    Add CKEditor to index.php for description textarea

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" 
+          crossorigin="anonymous">
+    <script src="https://cdn.ckeditor.com/4.18.0/standard/ckeditor.js"></script>
     <title>Simple Web Contact Form</title>
 </head>
 <body>
@@ -37,8 +39,11 @@
                     </div>
                     
                     <div class="form-group">
-                        <label for="description">Description</label>
+                        <label for="description">Description</label><br>
                         <textarea name="description" cols="30" rows="2"></textarea>
+                        <script>
+                            CKEDITOR.replace( 'description' );
+                        </script>
                     </div>
                     
                     <button class="btn btn-success">Submit</button>
