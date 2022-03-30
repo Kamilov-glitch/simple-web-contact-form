174352e0803df25a48c951652ff563d7cf57c155
Fuad <kamilovfuad@gmail.com>
2022-03-30 20:59:20 +0400

    Remove comments

--- index.php
+++ index.php
@@ -3,10 +3,3 @@
 include "partials/_form.php";
 
 ?>
-
-
-<?php
-// create validations for fields
-// if submitted info is alright redirect with header func
-// make sure to keep filled fields in case of error
-?>
