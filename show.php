b19b933a2c89a838f0f45746e2080ef939529b0b
Fuad <kamilovfuad@gmail.com>
2022-03-30 15:52:25 +0400

    Add table to show.php

--- show.php
+++ show.php
@@ -7,7 +7,7 @@ $email = '';
 $issue = '';
 $description = '';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') :
     if (
         isset($_POST['name']) &&
         isset($_POST['email']) &&
@@ -16,13 +16,40 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     ) {
         $name = $_SESSION['name'] = $_POST['name'];
         $email = $_SESSION['email'] = $_POST['email'];
-        $issue = $_SESSION['issue'] = $_POST['issue'];
+        $issue = ucfirst($_SESSION['issue'] = $_POST['issue']);
         $description = $_SESSION['description'] = $_POST['description'];
     }
+?>
+<div class="container">
+    <div class="card-header">
+        <h3>Your information: </h3>
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <tbody>
+                <tr>
+                    <th>Name</th>
+                    <td><?php echo $name?></td>
+                </tr>
+                <tr>
+                    <th>Email</th>
+                    <td><?php echo $email ?></td>
+                </tr>
+                <tr>
+                    <th>Issue</th>
+                    <td><?php echo $issue ?></td>
+                </tr>
+                <tr>
+                    <th>Descripiton</th>
+                    <td><?php echo $description ?></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
 
-    echo $name . "<br>";
-    echo $email . "<br>";
-    echo $issue . "<br>";
-    echo $description . "<br>";
-    echo "<button class='btn btn-success'><a href='edit.php'>Edit</a></button";
-}
+
+<?php    
+echo "<br>";
+echo "<button class='btn btn-success'><a href='edit.php'>Edit</a></button";
+endif; ?>
